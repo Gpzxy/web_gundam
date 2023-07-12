4ec297ae8636173065dbb0aeb6f01877071f36de
Gpzxy <galuhgaluhpakuan@gmail.com>
2023-07-12 12:47:14 +0700

    perbaikan kode reveal

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ menu.onclick = () => {
   navlist.classList.toggle("open");
 };
 
-const sr = scrollreveal({
+const sr = ScrollReveal({
   distance: "65px",
   duration: 2600,
   delay: 450,
@@ -15,5 +15,5 @@ const sr = scrollreveal({
 
 sr.reveal(".hero-text", { delay: 200, origin: "top" });
 sr.reveal(".hero-img", { delay: 450, origin: "top" });
-sr.reveal(".icons", { delay: 500, origin: "left" });
-sr.reveal(".scroll-down", { delay: 500, origin: "right" });
+sr.reveal(".icons", { delay: 200, origin: "left" });
+sr.reveal(".scroll-down", { delay: 200, origin: "right" });
